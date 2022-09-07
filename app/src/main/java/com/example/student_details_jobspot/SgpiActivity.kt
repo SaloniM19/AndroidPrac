63109180d9807a6ff2a67b5b461074e78636a6fa
Krish Parekh <Krishp4123@gmail.com>
2022-09-07 18:52:27 +0530

    Added Firebase and Firebase Firestore to code

--- app/src/main/java/com/example/student_details_jobspot/SgpiActivity.kt
+++ app/src/main/java/com/example/student_details_jobspot/SgpiActivity.kt
@@ -2,13 +2,18 @@ package com.example.student_details_jobspot
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.example.student_details_jobspot.databinding.ActivityAddressBinding
 import com.example.student_details_jobspot.databinding.ActivityMainBinding
 import com.example.student_details_jobspot.databinding.ActivitySgpiBinding
+import com.example.student_details_jobspot.model.User
+import com.google.firebase.firestore.FirebaseFirestore
 
+private const val TAG = "SGPI ACTIVITY"
 class SgpiActivity : AppCompatActivity() {
     private lateinit var binding1: ActivityAddressBinding
     private lateinit var binding2: ActivitySgpiBinding
+    private val mFirebaseStore : FirebaseFirestore by lazy { FirebaseFirestore.getInstance() }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_address)
@@ -19,16 +24,36 @@ class SgpiActivity : AppCompatActivity() {
         setContentView(binding1.root)
         setContentView(binding2.root)
 
-        val sapId = intent.getLongExtra("EXTRA_SapID", 0)
-        val mobile = intent.getLongExtra("EXTRA_Mobile", 0)
-        val dob = intent.getStringExtra("EXTRA_DOB")
-        val gender = intent.getStringExtra("EXTRA_Gender")
-        val address1 = intent.getStringExtra("EXTRA_Address1")
-        val address2 = intent.getStringExtra("EXTRA_Address2")
-        val city = intent.getStringExtra("EXTRA_City")
-        val state = intent.getStringExtra("EXTRA_State")
-        val zipcode = intent.getLongExtra("EXTRA_ZipCode",0)
+        val sapId = intent.getLongExtra("EXTRA_SapID", 0).toString()
+        val mobile = intent.getLongExtra("EXTRA_Mobile", 0).toString()
+        val dob = intent.getStringExtra("EXTRA_DOB").toString()
+        val gender = intent.getStringExtra("EXTRA_Gender").toString()
+        val address1 = intent.getStringExtra("EXTRA_Address1").toString()
+        val address2 = intent.getStringExtra("EXTRA_Address2").toString()
+        val city = intent.getStringExtra("EXTRA_City").toString()
+        val state = intent.getStringExtra("EXTRA_State").toString()
+        val zipcode = intent.getLongExtra("EXTRA_ZipCode",0).toString()
         val details = "$sapId $mobile $dob $gender $address1 $address2 $city $state $zipcode"
+
+        val detail = User(
+            sapId = sapId,
+            mobile = mobile,
+            dob = dob,
+            gender = gender,
+            address = "$address1 $address2",
+            city = city,
+            state = state,
+            zipCode = zipcode
+        )
+
+        mFirebaseStore.collection("user").document().set(detail)
+            .addOnSuccessListener {
+                Log.d(TAG, "We are goood")
+            }
+            .addOnFailureListener {
+                Log.d(TAG, "We are bad ${it.message}")
+            }
+
         binding2.tv2.text = details
     }
 }
---
+++ app/src/main/java/com/example/student_details_jobspot/model/User.kt
@@ -0,0 +1,12 @@
+package com.example.student_details_jobspot.model
+
+data class User(
+    var sapId : String = "",
+    var mobile: String = "",
+    var dob : String = "",
+    var gender : String = "",
+    var address : String = "",
+    var city : String = "",
+    var state : String = "",
+    var zipCode : String = "",
+)
