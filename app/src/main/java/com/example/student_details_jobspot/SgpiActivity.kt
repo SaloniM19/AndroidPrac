d78e84a41774f481e3d89710ed5fb08b9aeb7497
SALONI <salonimehta50289@gmail,com>
2022-09-07 20:18:00 +0530

    SgpiActivity input fields data taken

--- app/src/main/java/com/example/student_details_jobspot/SgpiActivity.kt
+++ app/src/main/java/com/example/student_details_jobspot/SgpiActivity.kt
@@ -1,8 +1,10 @@
 package com.example.student_details_jobspot
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import com.example.student_details_jobspot.databinding.ActivityAddressBinding
 import com.example.student_details_jobspot.databinding.ActivityMainBinding
 import com.example.student_details_jobspot.databinding.ActivitySgpiBinding
@@ -55,5 +57,15 @@ class SgpiActivity : AppCompatActivity() {
             }
 
         binding2.tv2.text = details
+
+        binding2.btnSgpiNext.setOnClickListener{
+            val sem1 = binding2.etSem1.text.toString()
+            val sem2 = binding2.etSem2.text.toString()
+            val sem3 = binding2.etSem3.text.toString()
+            val sem4 = binding2.etSem4.text.toString()
+            val avg = binding2.etAvg.text.toString()
+
+            Toast.makeText(this,"$sem1 $sem2 $sem3 $sem4 $avg" , Toast.LENGTH_SHORT).show()
+        }
     }
 }
